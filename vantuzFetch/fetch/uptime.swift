c7924a298a48b2c1dad24b0fb5653c2b650898df
showdima <admin@MacBook-Air-admin.local>
2026-05-31 00:00:28 +0300

    ref: uptime now confirms fetchableModule protocol

--- vantuzFetch/fetch/uptime.swift
+++ vantuzFetch/fetch/uptime.swift
@@ -4,4 +4,38 @@
 //
 //  Created by showydima on 30.05.2026.
 //
+import Foundation
 
+struct OSUptime: FetchableModule {
+    let id: String = "uptime"
+    var isFetched: Bool = false
+    var results: [FetchResult] = []
+    mutating func run() {
+        let uptime: TimeInterval = ProcessInfo.processInfo.systemUptime
+        var uptimeFormatted: String {
+            let formatter = DateComponentsFormatter()
+            formatter.unitsStyle = .abbreviated
+            formatter.zeroFormattingBehavior = .pad
+            
+            let oneDay: TimeInterval = 60 * 60 * 24
+            let oneMonth: TimeInterval = 60 * 60 * 24 * 30
+            
+            if uptime >= oneMonth { formatter.allowedUnits = [.month, .day, .hour]
+            } else 
+            if uptime >= oneDay { formatter.allowedUnits = [.day, .hour, .minute]
+            } else { formatter.allowedUnits = [.hour, .minute, .second]
+            }
+                
+            guard let formattedString = formatter.string(from: uptime) else { return "error while formating" }
+            
+            let regex = try? NSRegularExpression(pattern: "(\\d+)\\s+(\\w+)")
+            let range = NSRange(formattedString.startIndex..., in: formattedString)
+                
+            return regex?.stringByReplacingMatches(in: formattedString, 
+                                                options: [], 
+                                                range: range, 
+                                                withTemplate: "$1$2") ?? formattedString
+        }
+        self.results = [FetchResult(keyId: "uptime", value: uptimeFormatted)]
+    }
+}
