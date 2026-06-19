28daec0897f82909bc26c5a7b2b10a6fbd3efd02
showydima <->
2026-06-19 14:41:06 +0300

    ref: uptime.swift

--- vantuzFetch/fetch/uptime.swift
+++ vantuzFetch/fetch/uptime.swift
@@ -6,37 +6,74 @@
 //
 import Foundation
 
+let oneDay: TimeInterval = 60 * 60 * 24
+let oneMonth: TimeInterval = 60 * 60 * 24 * 30
+
+struct TimeModuleParser {
+    static let shared = TimeModuleParser()
+    
+    private let monthFormatter: DateComponentsFormatter
+    private let dayFormatter: DateComponentsFormatter
+    private let hourFormatter: DateComponentsFormatter
+    
+    private let regex = try? NSRegularExpression(pattern: "(\\d+)\\s+(\\w+)")
+    
+    private let oneMonth: TimeInterval = 2592000
+    private let oneDay: TimeInterval = 86400
+    
+    private init() {
+        let baseFormatter = { (units: NSCalendar.Unit) -> DateComponentsFormatter in
+            let f = DateComponentsFormatter()
+            f.unitsStyle = .abbreviated
+            f.zeroFormattingBehavior = .pad
+            f.allowedUnits = units
+            return f
+        }
+        
+        self.monthFormatter = baseFormatter([.month, .day, .hour])
+        self.dayFormatter = baseFormatter([.day, .hour, .minute])
+        self.hourFormatter = baseFormatter([.hour, .minute, .second])
+    }
+    
+    func formatTime(_ time: Int) -> String {
+        return formatTime(TimeInterval(time))
+    }
+    
+    func formatTime(_ time: TimeInterval) -> String {
+        let formatter: DateComponentsFormatter
+        if time >= oneMonth { formatter = monthFormatter }
+        else if time >= oneDay { formatter = dayFormatter }
+        else { formatter = hourFormatter }
+        
+        guard let formattedString = formatter.string(from: time) else {
+            return "error while formatting"
+        }
+        
+        let range = NSRange(formattedString.startIndex..., in: formattedString)
+        return regex?.stringByReplacingMatches(in: formattedString,
+                                               options: [],
+                                               range: range,
+                                               withTemplate: "$1$2") ?? formattedString
+    }
+}
 struct OSUptimeModule: FetchableModule {
     let id: String = "uptime"
     
     func run() -> [FetchResult] {
         var results: [FetchResult] = []
+        let parser = TimeModuleParser.shared
         let uptime: TimeInterval = ProcessInfo.processInfo.systemUptime
-        var uptimeFormatted: String {
-            let formatter = DateComponentsFormatter()
-            formatter.unitsStyle = .abbreviated
-            formatter.zeroFormattingBehavior = .pad
-            
-            let oneDay: TimeInterval = 60 * 60 * 24
-            let oneMonth: TimeInterval = 60 * 60 * 24 * 30
-            
-            if uptime >= oneMonth { formatter.allowedUnits = [.month, .day, .hour]
-            } else 
-            if uptime >= oneDay { formatter.allowedUnits = [.day, .hour, .minute]
-            } else { formatter.allowedUnits = [.hour, .minute, .second]
-            }
-                
-            guard let formattedString = formatter.string(from: uptime) else { return "error while formating" }
-            
-            let regex = try? NSRegularExpression(pattern: "(\\d+)\\s+(\\w+)")
-            let range = NSRange(formattedString.startIndex..., in: formattedString)
-                
-            return regex?.stringByReplacingMatches(in: formattedString, 
-                                                options: [], 
-                                                range: range, 
-                                                withTemplate: "$1$2") ?? formattedString
-        }
-        results = [FetchResult(keyId: self.id, value: uptimeFormatted)]
+        results = [FetchResult(keyId: self.id, value: parser.formatTime(uptime))]
         return results
+    }
+}
+
+struct WakeTimeModule: FetchableModule {
+    let id: String = "waketime"
+    
+    func run() -> [FetchResult] {
+        let waketime = sysctlTime("kern.waketime")
+        guard let waketime else { return [] }
+        return [FetchResult(keyId: "waketime", value: TimeModuleParser.shared.formatTime(Date.now.timeIntervalSince1970 - Double(waketime)))]
     }
 }
