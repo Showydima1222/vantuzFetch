3728303b7dee2cae298e6a5bcf454d3eff766400
showydima <->
2026-07-03 17:13:24 +0300

    ref: memory optimization on memory module

--- vantuzFetch/fetch/memory.swift
+++ vantuzFetch/fetch/memory.swift
@@ -8,75 +8,34 @@
 import Foundation
 import MachO
 
-class MemoryParser {
-    let stats: vm_statistics64?
-    let isParsed: Bool
-    init () {
+struct MemoryModule: FetchableModule {
+    let id: String = "mem"
+    let isHeavy: Bool = false
+       
+    func run() -> [FetchResult] {
         var stats = vm_statistics64()
-        var count = mach_msg_type_number_t(MemoryLayout.size(ofValue: stats) / MemoryLayout<integer_t>.size)
+        var count = mach_msg_type_number_t(MemoryLayout<vm_statistics64>.size / MemoryLayout<integer_t>.size)
         let hostPort = mach_host_self()
+        
         let result = withUnsafeMutablePointer(to: &stats) {
             $0.withMemoryRebound(to: integer_t.self, capacity: Int(count)) {
                 host_statistics64(hostPort, HOST_VM_INFO64, $0, &count)
             }
         }
-        if result == KERN_SUCCESS {
-            self.stats = stats
-            self.isParsed = true
-        } else {
-            self.isParsed = false
-            self.stats = nil
-        }
         
-    }
-}
-
-struct MemoryInfo {
-    let total: UInt64
-    let free: UInt64
-    let active: UInt64
-    let inactive: UInt64
-    let wired: UInt64
-    let speculative: UInt64
-    let compressed: UInt64
-    let external: UInt64
-    let purgable: UInt64
-    let swapped: UInt64
-    var usedMemory : UInt64 {
-        return total - (external + (free - speculative) )
-    }
-    init(stats: vm_statistics64, pageSize: UInt64) {
-        func calcBytes(pageCount: UInt64) -> UInt64 {
-            return pageCount * pageSize
-        }
-        self.total = UInt64(ProcessInfo.processInfo.physicalMemory)
-        self.free = calcBytes(pageCount: UInt64(stats.free_count))
-        self.active = calcBytes(pageCount: UInt64(stats.active_count))
-        self.inactive = calcBytes(pageCount: UInt64(stats.inactive_count))
-        self.wired = calcBytes(pageCount: UInt64(stats.wire_count))
-        self.speculative = calcBytes(pageCount: UInt64(stats.speculative_count))
-        self.compressed = calcBytes(pageCount: UInt64(stats.compressor_page_count))
-        self.external = calcBytes(pageCount: UInt64(stats.external_page_count))
-        self.purgable = calcBytes(pageCount: UInt64(stats.purgeable_count))
-        self.swapped = calcBytes(pageCount: stats.swapped_count)
-    }
-}
-
-
-struct MemoryModule: FetchableModule {
-    let id: String = "mem"
-       
-    func run() -> [FetchResult] {
-        var results: [FetchResult] = []
-        let parser = MemoryParser()
-        if parser.isParsed {
-            var pageSize: vm_size_t = 0
-            host_page_size(mach_host_self(), &pageSize)
-            let info = MemoryInfo(stats: parser.stats!, pageSize: UInt64(pageSize))
-            let totalGb: String = info.total.autoCS()
-            let usedGb = info.usedMemory.autoCS()
-            results = [FetchResult(keyId: self.id, value: "\(totalGb) total, \(usedGb) used")]
-        }
-        return results
+        guard result == KERN_SUCCESS else { return [] }
+        
+        var pageSize: vm_size_t = 0
+        host_page_size(hostPort, &pageSize)
+        let uPageSize = UInt64(pageSize)
+        
+        let total = UInt64(ProcessInfo.processInfo.physicalMemory)
+        let free = UInt64(stats.free_count) * uPageSize
+        let speculative = UInt64(stats.speculative_count) * uPageSize
+        let external = UInt64(stats.external_page_count) * uPageSize
+        
+        let usedMemory = total - (external + (free - speculative))
+        
+        return [FetchResult(keyId: self.id, value: "\(total.autoCS()) total, \(usedMemory.autoCS()) used")]
     }
 }
